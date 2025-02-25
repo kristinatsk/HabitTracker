68f09424e28dcd90ea0befbf331b33da0e3e2ed1
kristinatsk <kristinatsk@gmail.com>
2025-02-25 19:34:01 +0300

    Add habit view fixed

--- HabitTracker/AddHabitView.swift
+++ HabitTracker/AddHabitView.swift
@@ -22,18 +22,21 @@ struct AddHabitView: View {
                 TextField("Title", text: $habitTitle)
                 TextField("Description", text: $habitsDescription)
             }
-        }
-        .navigationTitle("Add item")
-        .toolbar {
-            Button("Save") {
-                let item = Habit(title: habitTitle, description: habitsDescription, completionCount: completionAmount)
-                habits.items.append(item)
-                dismiss()
+            
+            .navigationTitle("Add item")
+            .toolbar {
+                Button("Save") {
+                    let item = Habit(title: habitTitle, description: habitsDescription, completionCount: completionAmount)
+                    habits.items.append(item)
+                    dismiss()
+                }
             }
+            
         }
     }
 }
 
 #Preview {
     AddHabitView(habits: Habits())
+        .preferredColorScheme(.dark)
 }
